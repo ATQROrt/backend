932e8e071049c043e31e4d2744bbc49a6b2759cc
sambameli <santiago.sambataro@mercadolibre.com>
2019-09-03 20:12:03 -0300

    first commit, initializer

---
+++ src/main/java/com/ort/atqr/AtqrApplication.java
@@ -0,0 +1,13 @@
+package com.ort.atqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtqrApplication.class, args);
+	}
+
+}
