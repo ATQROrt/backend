fcf80b54e23c21ea75fb197c6b183fb5bf643a96
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-11 00:31:11 -0300

    Entity Professor

--- src/main/java/com/ort/atqr/Model/Professor.java
+++ src/main/java/com/ort/atqr/Model/Professor.java
@@ -1,10 +1,15 @@
 package com.ort.atqr.Model;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
 
+@Entity
 public class Professor extends User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @OneToMany(targetEntity=Course.class, fetch= FetchType.EAGER)
     private Set<Course> courses;
 
     public Professor(String firstName, String lastName, Long document, String mail, Date birth, String imageUrl, String password) {
