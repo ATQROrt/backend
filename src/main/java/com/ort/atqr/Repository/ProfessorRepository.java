633646545c3930ff9a345e4fee89eacbbc9fec05
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-11 00:45:36 -0300

    Commented out repositories

--- src/main/java/com/ort/atqr/Repository/ProfessorRepository.java
+++ src/main/java/com/ort/atqr/Repository/ProfessorRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ProfessorRepository extends CrudRepository<Professor, Long> {
-    Professor findBydocument(Long document);
-    List<Professor> findByfirstNameAndlastNameOrderBydocument(String firstName, String lastName);
+   // Professor findBydocument(Long document);
+   // List<Professor> findByfirstNameAndlastNameOrderBydocument(String firstName, String lastName);
 }
--- src/main/java/com/ort/atqr/Repository/StudentRepository.java
+++ src/main/java/com/ort/atqr/Repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
-    Student findByDocument(Long document);
-    List<Student> findByfirstNameAndlastNameOrderBydocument(String firstName, String lastName);
-    Student findStudentBydocumentAndpassword(Long document, String password);
+    //Student findByDocument(Long document);
+    //List<Student> findByfirstNameAndlastNameOrderBydocument(String firstName, String lastName);
+    //Student findStudentBydocumentAndpassword(Long document, String password);
 }
