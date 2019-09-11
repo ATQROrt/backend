fee87a0d50a3444ac9d75d1439d6b588318e4f6e
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-10 23:29:11 -0300

    Add ID all entities

--- src/main/java/com/ort/atqr/Model/Course.java
+++ src/main/java/com/ort/atqr/Model/Course.java
@@ -1,11 +1,16 @@
 package com.ort.atqr.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
 public class Course {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String code;
     private Asignature asignature;
     private List<Student> studentsList;
