2bb0fa5aa1e39dbc5c27e2ecf36a562e48827ce5
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-11 00:43:44 -0300

    ?

--- src/main/java/com/ort/atqr/Model/Student.java
+++ src/main/java/com/ort/atqr/Model/Student.java
@@ -15,4 +15,6 @@ public class Student extends User {
     public Student(String firstName, String lastName, Long document, String mail, Date birth, String imageUrl, String password) {
         super(firstName, lastName, document, mail, birth, imageUrl, password);
     }
+
+
 }
--- src/main/java/com/ort/atqr/Repository/StudentRepository.java
+++ src/main/java/com/ort/atqr/Repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
-    Student findBydocument(Long document);
-    List<Student> findByfirstNameAndlastNameOrderByDocument(String firstName, String lastName);
+    Student findByDocument(Long document);
+    List<Student> findByfirstNameAndlastNameOrderBydocument(String firstName, String lastName);
     Student findStudentBydocumentAndpassword(Long document, String password);
 }
