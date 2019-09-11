ff27ae07edc521e5ccc33d62cbdcdfc00f061b1f
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-11 00:38:59 -0300

    fix error idk

--- src/main/java/com/ort/atqr/Model/Course.java
+++ src/main/java/com/ort/atqr/Model/Course.java
@@ -6,9 +6,8 @@ import java.util.Set;
 @Entity
 public class Course {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String code;
+    @OneToOne
     private Asignature asignature;
     @OneToMany(targetEntity=Student.class, fetch= FetchType.EAGER)
     private Set<Student> studentsList;
@@ -16,4 +15,43 @@ public class Course {
     @OneToMany(targetEntity=ClassDay.class, fetch= FetchType.EAGER)
     private Set<ClassDay> classDayList;
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Asignature getAsignature() {
+        return asignature;
+    }
+
+    public void setAsignature(Asignature asignature) {
+        this.asignature = asignature;
+    }
+
+    public Set<Student> getStudentsList() {
+        return studentsList;
+    }
+
+    public void setStudentsList(Set<Student> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public Set<ClassDay> getClassDayList() {
+        return classDayList;
+    }
+
+    public void setClassDayList(Set<ClassDay> classDayList) {
+        this.classDayList = classDayList;
+    }
 }
