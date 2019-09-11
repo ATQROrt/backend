9ba0114eaa355c9d714cc75c8e64db31d9eb3ad0
Martin Kiperszmid <mkiperszmid@gmail.com>
2019-09-10 23:38:45 -0300

    Add ID prof and stud

--- src/main/java/com/ort/atqr/Model/Professor.java
+++ src/main/java/com/ort/atqr/Model/Professor.java
@@ -1,12 +1,17 @@
 package com.ort.atqr.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 public class Professor extends User{
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private List<Course> courses;
 
     public Professor(String firstName, String lastName, Long document, String mail, Date birth, String imageUrl, String password) {
--- src/main/java/com/ort/atqr/Model/User.java
+++ src/main/java/com/ort/atqr/Model/User.java
@@ -1,16 +1,11 @@
 package com.ort.atqr.Model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import java.util.Date;
 
 public abstract class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+
     private String firstName;
     private String lastName;
     private Long document;
@@ -33,10 +28,6 @@ public abstract class User {
     @PrePersist
     private void createdAt(){
         this.createdAt = new Date();
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public Date getCreatedAt() {
