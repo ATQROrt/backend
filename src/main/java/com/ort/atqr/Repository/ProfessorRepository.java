4e60c72549430f8af4dbbbf0e8688a6aee503e1f
sambameli <santiago.sambataro@mercadolibre.com>
2019-11-19 20:20:01 -0300

    Fix prof repository

--- src/main/java/com/ort/atqr/Repository/ProfessorRepository.java
+++ src/main/java/com/ort/atqr/Repository/ProfessorRepository.java
@@ -2,9 +2,11 @@ package com.ort.atqr.Repository;
 
 import com.ort.atqr.Model.Professor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProfessorRepository extends CrudRepository<Professor, Long> {
     Professor findProfessorByDocumentAndPassword(Long document, String password);
 
